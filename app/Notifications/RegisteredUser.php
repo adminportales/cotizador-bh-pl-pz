a55c5620ad9860a8d94ef07bc72d2b9f0240ee20
Antonio Tomas <adminportales@promolife.com.mx>
2022-12-08 16:17:57 -0600

    copia del correo datos de acceso

--- app/Notifications/RegisteredUser.php
+++ app/Notifications/RegisteredUser.php
@@ -43,6 +43,7 @@ class RegisteredUser extends Notification
         return (new MailMessage)
             ->markdown('mail.registered.user', ['url' => url('/'), 'data' => $this->data])
             ->subject('Usuario Registrado al sistema Cotizador Web Promo Connect')
+            ->cc(['federico.solano@promolife.com.mx', 'antonio.tomas@promolife.com.mx'])
             ->from('innovacionytecnologia@promolife.online', 'Promo Connect');
     }
 
