ed08877a8a124358a5479196f5a7896aef0877d1
samantha-spindola <gisellesamspindola@gmail.com>
2023-11-16 14:04:05 -0600

    modificaciones en previsualizar el pdf

--- resources/views/cotizador/ver_cotizacion/create-presentation-component.blade.php
+++ resources/views/cotizador/ver_cotizacion/create-presentation-component.blade.php
@@ -1,54 +1,58 @@
-<div>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="form-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="portadaFile" wire:model="portada" accept="image/*">
-                    <label class="custom-file-label" for="portadaFile">Seleccionar Portada</label>
-                </div>
-                @error('portada')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-            {{-- Check para mostrar o no la contraportada --}}
-            <div class="form-group">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" {{ $tieneContraportada ? 'checked' : '' }}
-                        wire:model="tieneContraportada">
-                    <label class="form-check-label" style="font-weight: bold;" for="defaultCheck1">
-                        ¿Deseas una contraportada?
-                    </label>
-                </div>
-                @if ($tieneContraportada)
+<div class="flex">
+    <div class="w-1/2">
+        <div class="row ">
+            <div class="col-md-4 ">
+                <div class="form-group">
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="contraportadaFile"
-                            wire:model="contraportada" accept="image/*">
-                        <label class="custom-file-label" for="contraportadaFile">Seleccionar Contraportada</label>
+                        <input type="file" class="custom-file-input" id="portadaFile" wire:model="portada"
+                            accept="image/*">
+                        <label class="custom-file-label" for="portadaFile">Seleccionar Portada</label>
                     </div>
-                @endif
-                @error('contraportada')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="form-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="fondoFile" wire:model="fondo" accept="image/*">
-                    <label class="custom-file-label" for="fondoFile">Seleccionar Fondo</label>
+                    @error('portada')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
-                @error('fondo')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="form-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="logoFile" wire:model="logo" accept="image/*">
-                    <label class="custom-file-label" for="logoFile">Seleccionar Logo</label>
+                {{-- Check para mostrar o no la contraportada --}}
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" {{ $tieneContraportada ? 'checked' : '' }}
+                            wire:model="tieneContraportada">
+                        <label class="form-check-label" style="font-weight: bold;" for="defaultCheck1">
+                            ¿Deseas una contraportada?
+                        </label>
+                    </div>
+                    @if ($tieneContraportada)
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="contraportadaFile"
+                                wire:model="contraportada" accept="image/*">
+                            <label class="custom-file-label" for="contraportadaFile">Seleccionar Contraportada</label>
+                        </div>
+                    @endif
+                    @error('contraportada')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
-                @error('logo')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-            {{-- <div class="form-group">
+                <div class="form-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="fondoFile" wire:model="fondo"
+                            accept="image/*">
+                        <label class="custom-file-label" for="fondoFile">Seleccionar Fondo</label>
+                    </div>
+                    @error('fondo')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="logoFile" wire:model="logo"
+                            accept="image/*">
+                        <label class="custom-file-label" for="logoFile">Seleccionar Logo</label>
+                    </div>
+                    @error('logo')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                {{-- <div class="form-group">
                 <label for="color_primario">Color Primario</label>
                 <input type="color" class="form-control form-control-sm" id="color_primario"
                     wire:model="color_primario">
@@ -56,14 +60,14 @@
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div> --}}
-            {{-- <div class="form-group">
+                {{-- <div class="form-group">
                         <label for="color_secundario">Color Secundario</label>
                         <input type="color" class="form-control" id="color_secundario" wire:model="color_secundario">
                         @error('color_secundario')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div> --}}
-            {{-- <div class="form-group">
+                {{-- <div class="form-group">
                 <label for="productos_por_pagina">Productos Por Pagina</label>
                 <select name="" id="" wire:model="productos_por_pagina" class="form-control">
                     <option value="1">1</option>
@@ -74,7 +78,7 @@
                 @enderror
             </div> --}}
 
-            {{-- @php
+                {{-- @php
                 $subtotalAdded = 0;
                 $subtotalSubstract = 0;
                 $sumPrecioTotal = 0;
@@ -141,28 +145,31 @@
             @else
                 <p>No hay Productos Cotizados</p>
             @endif --}}
-            <button class="btn btn-info btn-sm btn-block mb-1" onclick="preview()">Previsualizar Presentacion</button>
-            {{-- <button class="btn btn-success btn-sm btn-block mb-1">Guardar Presentacion</button> --}}
-        </div>
-        <div class="col-md-8">
-            {{-- Spinnner loading --}}
-            <div wire:loading.flex wire:target="previewPresentation" class="justify-content-center">
-                <div class="spinner-border text-primary" role="status">
-                    <span class="sr-only">Loading...</span>
-                </div>
+                <button class="btn btn-info btn-sm btn-block mb-1" onclick="preview()">Previsualizar
+                    Presentacion</button>
+                {{-- <button class="btn btn-success btn-sm btn-block mb-1">Guardar Presentacion</button> --}}
             </div>
-            @if ($urlPDFPreview)
-                <iframe src="{{ $urlPDFPreview }}" style="width:100%; height:600px;" frameborder="0"></iframe>
-            @endif
         </div>
     </div>
-    <script>
-        function preview() {
-            @this.previewPresentation()
-        }
+    <div class="w-1/2 ">
+        {{-- Spinnner loading --}}
+        <div wire:loading.flex wire:target="previewPresentation" class="justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        @if ($urlPDFPreview)
+            <iframe src="{{ $urlPDFPreview }}" style="width:100%; height:600px;" frameborder="0"></iframe>
+        @endif
+    </div>
+</div>
+<script>
+    function preview() {
+        @this.previewPresentation()
+    }
 
-        function cerrarPreview() {
-            @this.urlPDFPreview = null;
-        }
-    </script>
+    function cerrarPreview() {
+        @this.urlPDFPreview = null;
+    }
+</script>
 </div>
