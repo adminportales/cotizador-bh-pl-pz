e052d1ae112f869238c6b57ff5f376feb9c29a1e
unknown <2519360028fhernandezc@gmail.com>
2024-02-16 10:05:54 -0600

    Se implementa un checkbox en la vista Finalizar-cotizacion, que permite al usuario elegir si desea enviar la cotización por correo electrónico al guardar la cotización.
    Cambios:
    
    Se ha añadido un checkbox a la vista Finalizar-cotizacion con el nombre enviar_correo.
    Se ha modificado la función enviar para obtener el valor del checkbox.
    Se ha pasado el valor del checkbox a la función guardarCotizacion.
    La función guardarCotizacion ahora comprueba el valor del checkbox y envía la cotización al correo electrónico del cliente si está marcado.

--- resources/views/cotizador/finalizar_cotizacion/finalizar-cotizacion.blade.php
+++ resources/views/cotizador/finalizar_cotizacion/finalizar-cotizacion.blade.php
@@ -130,6 +130,8 @@
 
 
                                     <div class="flex flex-col gap-2">
+                                        <input type="checkbox" id="enviar_correo" name="enviar_correo" class="mr-2">
+                                        <label for="enviar_correo">Enviar PDF por correo electrónico</label>
                                         <button class="bg-gray-200 p-3 rounded-md hover:bg-gray-300"
                                             data-modal-target="preview" data-modal-toggle="preview"
                                             onclick="preview()">Previsualizar Cotizacion</button>
@@ -282,9 +284,10 @@
         }
 
         function enviar() {
+            const enviarCorreo = document.getElementById('enviar_correo').checked;
             Swal.fire({
                 title: '¿Desea confirmar la cotización?',
-                html: "{{ auth()->user()->companySession->name }}<br><br>Se enviará una copia de la cotización al correo electrónico establecido y se registra como un lead nuevo en Odoo.",
+                html: {{ auth()->user()->companySession->name }}"<br><br>Se enviará una copia de la cotización al correo electrónico establecido y se registra como un lead nuevo en Odoo.",
                 showCancelButton: true,
                 icon: 'warning',
                 confirmButtonText: 'Guardar',
@@ -292,7 +295,7 @@
             }).then((result) => {
                 /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
-                    @this.guardarCotizacion()
+                    @this.guardarCotizacion(enviarCorreo)
                 } else {
                     Swal.fire('No se realizo ningun cambio', '', 'info')
                 }
